31f2a342283be690d3e7263f4b1e1c7c85436368
Andy <russellandy98@gmail.com>
2019-09-21 17:01:55 -0400

    implemented PatientManager class

--- project/src/Patient.java
+++ project/src/Patient.java
@@ -1,3 +1,31 @@
 public class Patient {
+    private Appointment appointment;
+    public String firstName, lastName;
+    public int SSN;
 
+    public Patient(String firstName, String lastName, int SSN) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.SSN = SSN;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSSN() {
+        return SSN;
+    }
 }
---
+++ project/src/PatientManager.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class PatientManager {
+    ArrayList<Patient> patients = new ArrayList();
+
+    public void addPatient(Patient patient){
+        patients.add(patient);
+    }
+
+    public int getPatient(Patient patient){
+        int SSN = patient.getSSN();
+        for (int i = 0; i < patients.size(); i++){
+            if(SSN == patients.get(i).getSSN()){
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    public void removePatient(Patient p){
+        patients.remove(p);
+    }
+}
