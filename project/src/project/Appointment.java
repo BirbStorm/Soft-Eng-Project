f01b515d48e93c4c5ca2131046d6804abcb1b9a1
Carson Davis <carsondavis340@gmail.com>
2019-09-23 12:00:58 -0400

    This is test 2 of making sure my eclipse and github are working properly. I can then move on to programming with Johnnie.

--- project/src/project/Appointment.java
+++ project/src/project/Appointment.java
@@ -3,7 +3,7 @@ package project;
 public class Appointment {
 
 	public static void main(String [] args) {
-		System.out.print("Testing pushing to my branch.");
+		System.out.print("This is test 2 of making sure my push is working.");
 	}
 	
 }
