5f0c17ddcb04bcb5e2a2e378e712631763848d5d
jboldfield <jboldfield@valdosta.edu>
2019-10-09 17:52:06 -0400

    Added Javadoc to Appointment Manager  class.

--- project/src/project/AppointmentManager.java
+++ project/src/project/AppointmentManager.java
@@ -1,15 +1,26 @@
 package project;
 
 import java.util.ArrayList;
-
+    /** 
+     * A class to keep track of all current apointments. Is managed
+     * by Receptionist who can add and remove appointments from the
+     * list.
+     */
 public class AppointmentManager {
-
     private ArrayList<Appointment> appointments = new ArrayList<>();
 
+    
+    /** 
+     * @param appointment
+     */
     public void addAppointment(Appointment appointment) {
         appointments.add(appointment);
     }
 
+    
+    /** 
+     * @param appointment
+     */
     public void removeAppointment(Appointment appointment) {
         if (appointments.contains(appointment)) {
             appointments.remove(appointment);
@@ -18,6 +29,11 @@ public class AppointmentManager {
         }
     }
 
+    
+    /** 
+     * @param appointment
+     * @return Appointment
+     */
     public Appointment getAppointment(Appointment appointment){
         for (Appointment a: appointments) {
             if(a.equals(appointment)){
