8a66f7b486aedc69838a4781f2653911a8285003
jboldfield <jboldfield@valdosta.edu>
2019-11-24 16:10:11 -0500

    Changed file structure

---
+++ src/DoctorController.java
@@ -0,0 +1,23 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.collections.*;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+import javafx.scene.input.MouseEvent;
+import javafx.util.Callback;
+import javafx.event.EventHandler;
+
+import java.sql.*;
+import java.util.*;
+
+public class DoctorController{
+    @FXML private TextArea patientName;
+    @FXML private TextArea roomNumber;
+    @FXML private TextArea patientInfo;
+    @FXML private TextArea symptoms;
+    @FXML private TextArea diagnosis;
+    @FXML private TextArea medicine;
+
+}
